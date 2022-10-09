1198576d4832c65334b22e61ebc20cfe0d1ff6c5
Marja-Leena <marjaleena.kiik@mail.utoronto.ca>
2022-10-09 12:09:31 -0400

    First attempt at completing the TODOs in DrivableMap, DrivableTrader, and Trader.

--- src/main/java/DrivableMap.java
+++ src/main/java/DrivableMap.java
@@ -27,6 +27,15 @@ class DrivableMap {
      *       in drivable_map, then add the pair to drivable_map.
      *       Return true if the Drivable was added to drivable_map.
      */
+    public boolean addDrivable(String key, Drivable item) {
+        if (this.drivable_map.containsKey(key)) {
+            return false;
+        }
+        else {
+            this.drivable_map.put(key, item);
+            return true;
+        }
+    }
 
 
 
@@ -37,6 +46,14 @@ class DrivableMap {
      * You may want to use drivable_map.keys() or drivable_map.values() to
      * iterate through drivable_map.
      */
+    public boolean hasFasterThan(int speed) {
+        for (Drivable object : this.drivable_map.values()) {
+            if (object.getMaxSpeed() >= speed) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 
 
@@ -46,6 +63,15 @@ class DrivableMap {
      *       returns a List containing all of the Tradable items in
      *       drivable_map.
      */
+    public List<Tradable> getTradable() {
+        List<Tradable> items = new ArrayList<Tradable>();
+        for (Drivable object : this.drivable_map.values()) {
+            if (object instanceof Tradable) {
+                items.add((Tradable) object);
+            }
+        }
+        return items;
+    }
 
 
 
--- src/main/java/DrivableTrader.java
+++ src/main/java/DrivableTrader.java
@@ -11,3 +11,39 @@
  */
 
 import java.util.List;
+
+public class DrivableTrader extends Trader<Drivable> {
+
+    /**
+     * Construct a DrivableTrader, giving them the given inventory,
+     * wishlist, and money.
+     *
+     * @param inventory Objects in the DrivableTrader's inventory
+     * @param wishlist Objects in this DrivableTrader's wishlist
+     * @param money    The DrivableTrader's money
+     */
+    public DrivableTrader(List<Drivable> inventory, List<Drivable> wishlist, int money) {
+        super(inventory, wishlist, money);
+    }
+
+    /**
+     * Construct a DrivableTrader, giving them the given money,
+     * and an empty wishlist and inventory.
+     *
+     * @param money   The DrivableTrader's money
+     */
+    public DrivableTrader(int money) {
+        super(money);
+    }
+
+    @Override
+    public int getSellingPrice(Drivable thing) {
+        int originalPrice = super.getSellingPrice(thing);
+        if (originalPrice == Tradable.MISSING_PRICE) {
+            return originalPrice;
+        }
+        else {
+            return originalPrice + thing.getMaxSpeed();
+        }
+    }
+}
